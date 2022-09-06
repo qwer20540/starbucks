e6871f4a2995d31f60860d2509d887cbd8e3d90f
JAEhun <87799246+qwer20540@users.noreply.github.com>
2022-09-06 13:46:35 +0900

    main_js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// search
 const searchEl = document.querySelector('.search');
 const searchIputEl = searchEl.querySelector('input');
 
@@ -13,4 +14,37 @@ searchIputEl.addEventListener('focus', function(){
 searchIputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchIputEl.setAttribute('placeholder', '');
+});
+
+
+
+// badges
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle(함수, 시간)
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    gsap.to(badgeEl, .5, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .5, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+
+
+//gsap.to(요소, 지속시간, 옵션)  애니메이션 js
+//hero img fadein
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay:(index + 1) * .7,
+    opacity: 1
+  });
 });
